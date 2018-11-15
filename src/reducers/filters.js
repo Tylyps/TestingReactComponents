e1c39129ccbc0129d501a198fc94b95f709436ac
Tylyps <j.chmielewski@o2.pl>
2018-11-15 21:19:48 +0100

    Create test for filters

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -9,7 +9,7 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch(action.type){
-    case 'SET_TEXT':
+    case 'SET_TEXT_FILTER':
       return {
         ...state,
         text: action.text
